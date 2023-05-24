8ebd8c3afb871e18c50657320656d854c48a2f1f
donPuerto <don.puerto@hotmail.com>
2023-05-24 10:43:45 +0800

    added changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Supabase App addtional',
+      title: 'Flutter Supabase App addtional testing',
       theme: ThemeData(
         // This is the theme of your application.
         //
